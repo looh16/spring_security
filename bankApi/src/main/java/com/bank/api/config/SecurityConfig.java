8c9e59a02f26002807025a31ddae50d62caeb924
clanga <[Your>
2023-03-13 14:02:33 +0200

    Add custom authentication

--- bankApi/src/main/java/com/bank/api/config/SecurityConfig.java
+++ bankApi/src/main/java/com/bank/api/config/SecurityConfig.java
@@ -1,14 +1,13 @@
 package com.bank.api.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
@@ -24,21 +23,6 @@ public class SecurityConfig {
 		.httpBasic();
 		
 		return http.build();
-	}
-	
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		
-		UserDetails admin = User.withUsername("admin")
-				.password("123456")
-				.authorities("admin")
-				.build();
-		
-		UserDetails user = User.withUsername("user")
-				.password("123456")
-				.authorities("read")
-				.build();
-		return new InMemoryUserDetailsManager(admin, user);
 	}
 	
 	@Bean
---
+++ bankApi/src/main/resources/sql/scripts.sql
@@ -0,0 +1,36 @@
+--Database creation
+
+create database spring_auth;
+use spring_auth;
+
+--Default autentication
+
+CREATE TABLE `users` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`username` VARCHAR(45) NOT NULL,
+`password` VARCHAR(45) NOT NULL,
+`enabled` INT NOT NULL,
+PRIMARY KEY (`id`));
+
+CREATE TABLE `authorities` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `authority` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+INSERT IGNORE INTO `users` VALUES (NULL, 'happy', '12345', '1');
+INSERT IGNORE INTO `authorities` VALUES (NULL, 'happy', 'write');
+
+
+--Custom autentication
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `pwd` varchar(200) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `customer` (`email`, `pwd`, `role`)
+ VALUES ('johndoe@example.com', '54321', 'admin');
