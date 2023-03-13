be9aa00ab409b31203ffa74fe63f02c33846e18b
clanga <[Your>
2023-03-13 16:36:04 +0200

    encrypt password

--- bankApi/src/main/java/com/bank/api/controllers/UserController.java
+++ bankApi/src/main/java/com/bank/api/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.bank.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +15,18 @@ import com.bank.api.repository.CustomerRepository;
 public class UserController {
 
 	@Autowired
-	CustomerRepository customerRepository;
+	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	 @PostMapping("/register")
 	    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
 	        Customer savedCustomer = null;
 	        ResponseEntity<String> response = null;
 	        try {
+	        	String hashPwd = passwordEncoder.encode(customer.getPwd());
+	        	customer.setPwd(hashPwd);
 	            savedCustomer = customerRepository.save(customer);
 	            if (savedCustomer.getId() > 0) {
 	                response = ResponseEntity
