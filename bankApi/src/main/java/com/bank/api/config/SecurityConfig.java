47c2be66ffbdc409b23eb0f828167b825d0251b9
looh16 <langacustodio@gmail.com>
2023-03-13 08:14:42 +0200

    Update security configurations

--- bankApi/src/main/java/com/bank/api/config/SecurityConfig.java
+++ bankApi/src/main/java/com/bank/api/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,18 +29,21 @@ public class SecurityConfig {
 	@Bean
 	public InMemoryUserDetailsManager userDetailsService() {
 		
-		UserDetails admin = User.withDefaultPasswordEncoder()
-				.username("admin")
+		UserDetails admin = User.withUsername("admin")
 				.password("123456")
 				.authorities("admin")
 				.build();
 		
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user")
+		UserDetails user = User.withUsername("user")
 				.password("123456")
 				.authorities("read")
 				.build();
 		return new InMemoryUserDetailsManager(admin, user);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
